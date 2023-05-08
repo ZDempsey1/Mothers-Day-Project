a3948ad770206988343bce97e1c395d8b20d3047
zach <zachartydempsey@gmail.com.here>
2023-05-08 13:52:28 -0500

    favorites button updated

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -41,9 +41,17 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({ flowerId }) => {
   const Icon = isFavorite ? CheckIcon : PlusIcon;
 
   return (
+    <>
     <div onClick={toggleFavorites} className="cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
       <Icon className="text-white group-hover/item:text-neutral-300 w-4 lg:w-6" />
     </div>
+    <div>
+      <span className="text-white text-xs ml-1 lg:text-sm">
+        {isFavorite ? 'Remove from favorites' : 'Add to favorites'}
+      </span>
+    </div>
+    </>
+
   )
 }
 
--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -15,11 +15,9 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
   const router = useRouter();
   const { openModal } = useInfoModalStore();
 
-  const redirectToWatch = useCallback(() => router.push(`/watch/${data.id}`), [router, data.id]);
-
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
-      <img onClick={redirectToWatch} src={data.pic1} alt="Movie" draggable={false} className="
+      <img src={data.pic1} alt="" draggable={false} className="
         cursor-pointer
         object-cover
         transition
@@ -28,7 +26,6 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
         rounded-md
         group-hover:opacity-90
         sm:group-hover:opacity-0
-        delay-300
         w-full
         h-[12vw]
       " />
@@ -49,7 +46,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
         group-hover:translate-x-[2vw]
         group-hover:opacity-100
       ">
-        <img onClick={redirectToWatch} src={data.pic2} alt="Movie" draggable={false} className="
+        <img src={data.pic2} alt="" draggable={false} className="
           cursor-pointer
           object-cover
           transition
@@ -71,23 +68,20 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
           rounded-b-md
           ">
           <div className="flex flex-row items-center gap-3">
-            <div onClick={redirectToWatch} className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300">
-              <PlayIcon className="text-black w-4 lg:w-6" />
-            </div>
             <FavoriteButton flowerId={data.id} />
             <div onClick={() => openModal(data?.id)} className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
               <ChevronDownIcon className="text-white group-hover/item:text-neutral-300 w-4 lg:w-6" />
             </div>
           </div>
           <p className="text-green-400 font-semibold mt-4">
-            New <span className="text-white">2023</span>
-          </p>
-          <div className="flex flex-row mt-4 gap-2 items-center">
-            <p className="text-white text-[10px] lg:text-sm">{data.duration}</p>
-          </div>
-          <div className="flex flex-row items-center gap-2 mt-4 text-[8px] text-white lg:text-sm">
-            <p>{data.genre}</p>
-          </div>
+  Common name <span className="text-white">{data.name}</span>
+</p>
+<div className="flex flex-row mt-4 gap-2 items-center">
+  <p className="text-green-400 font-semibold text-[10px] lg:text-sm">
+    Type of plant <span className="text-white">{data.plantType}</span>
+  </p>
+</div>
+
         </div>
       </div>
     </div>
