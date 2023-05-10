886d4fb096c8a1d16b718a4f50a09544f8a36517
zach <zachartydempsey@gmail.com.here>
2023-05-09 19:14:33 -0500

    plant id page working

--- pages/api/flowers/[flowerId].ts
+++ pages/api/flowers/[flowerId].ts
@@ -1,35 +1,64 @@
-import { NextApiRequest, NextApiResponse } from "next";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import { PrismaClient } from '@prisma/client';
+// import serverAuth from "@/libs/serverAuth";
+
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//     try {
+//         if (req.method !== 'GET') {
+//         return res.status(405).end();
+//         }
+
+//         await serverAuth(req, res);
+
+//         const { flowerId } = req.query;
+
+//         if (typeof flowerId !== 'string') {
+//         throw new Error('Invalid Id');
+//         }
+
+//         if (!flowerId) {
+//         throw new Error('Missing Id');
+//         }
+//         const prisma = new PrismaClient();
+//         const flowers = await prisma.flower.findUnique({
+//             where: {
+//                 id: flowerId,
+//             },
+//         });
+
+
+//         return res.status(200).json(flowers);
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(500).end();
+//     }
+// }
 import { PrismaClient } from '@prisma/client';
-import serverAuth from "@/libs/serverAuth";
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        if (req.method !== 'GET') {
-        return res.status(405).end();
-        }
+    const flowerId = req.query.flowerId;
 
-        await serverAuth(req, res);
-
-        const { flowerId } = req.query;
-
-        if (typeof flowerId !== 'string') {
-        throw new Error('Invalid Id');
-        }
-
-        if (!flowerId) {
-        throw new Error('Missing Id');
-        }
-        const prisma = new PrismaClient();
-        const flowers = await prisma.flower.findUnique({
+    if (req.method === 'GET') {
+        try {
+        const flower = await prisma.flower.findUnique({
             where: {
-                id: flowerId,
+            id: String(flowerId),
             },
         });
 
+        if (!flower) {
+            return res.status(404).json({ error: 'Flower not found' });
+        }
 
-        return res.status(200).json(flowers);
-    } catch (error) {
-        console.log(error);
-        return res.status(500).end();
+        return res.status(200).json(flower);
+        } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'An error occurred while fetching the flower data' });
+        }
+    } else {
+        return res.status(405).json({ error: 'Method not allowed' });
     }
 }
--- pages/plants/[plantId].tsx
+++ pages/plants/[plantId].tsx
@@ -1,10 +1,130 @@
-// pages/plants/[plantId].tsx
+// // pages/plants/[plantId].tsx
+// import { FlowerInterface } from '@/types';
+// import { useRouter } from 'next/router';
+// import { GetServerSideProps } from 'next';
+// import axios from 'axios';
 
+// interface FlowerDetailsProps {
+//   flower: FlowerInterface;
+// }
+
+
+// const FlowerDetails: React.FC<FlowerDetailsProps> = ({ flower }) => {
+//   const router = useRouter();
+
+//   if (router.isFallback) {
+//     return <div>Loading...</div>;
+//   }
+
+//   // Display flower details using the flower prop
+//   return (
+//     <div>
+//       <h1>{flower.name}</h1>
+//       {/* Add more styling and content here */}
+//     </div>
+//   );
+// }
+
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const flowerId = context.params?.flowerId;
+
+//   if (!flowerId) {
+//     return {
+//       notFound: true,
+//     };
+//   }
+
+//   try {
+//     const response = await axios.get(`/api/flowers/${flowerId}`);
+//     const flower = response.data;
+
+//     return {
+//       props: {
+//         flower,
+//       },
+//     };
+//   } catch (error) {
+//     console.error(error);
+
+//     return {
+//       notFound: true,
+//     };
+//   }
+// };
+
+// export default FlowerDetails;
+
+
+
+
+
+// import { FlowerInterface } from '@/types';
+// import { useRouter } from 'next/router';
+// import { GetServerSideProps } from 'next';
+// import axios from 'axios';
+
+// interface FlowerDetailsProps {
+//   flower: FlowerInterface;
+// }
+
+// const FlowerDetails: React.FC<FlowerDetailsProps> = ({ flower }) => {
+//     const router = useRouter();
+
+//   if (router.isFallback) {
+//     return <div>Loading...</div>;
+//   }
+
+//   // Display flower details using the flower prop
+//   return (
+//     <div>
+//       <h1>{flower.name}</h1>
+//       {/* Add more styling and content here */}
+//     </div>
+//   );
+// }
+
+
+// export const getStaticProps: GetStaticProps = async (context) => {
+//   const flowerId = context.params?.flowerId;
+
+//   if (!flowerId) {
+//     return {
+//       notFound: true,
+//     };
+//   }
+
+//   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+//   try {
+//     const response = await axios.get(`${apiUrl}/api/flowers/${flowerId}`);
+//     const flower = response.data;
+
+//     return {
+//       props: {
+//         flower,
+//       },
+//     };
+//   } catch (error) {
+//     console.error(error);
+
+//     return {
+//       notFound: true,
+//     };
+//   }
+// };
+
+// export default FlowerDetails;
+
+
+import { FlowerInterface } from '@/types';
 import { useRouter } from 'next/router';
-import { GetServerSideProps } from 'next';
-import axios from 'axios';
+import { GetStaticProps, GetStaticPaths } from 'next';
 
-const FlowerDetails = ({ flower }) => {
+interface FlowerDetailsProps {
+  flower: FlowerInterface;
+}
+
+const FlowerDetails: React.FC<FlowerDetailsProps> = ({ flower }) => {
   const router = useRouter();
 
   if (router.isFallback) {
@@ -18,10 +138,10 @@ const FlowerDetails = ({ flower }) => {
       {/* Add more styling and content here */}
     </div>
   );
-}
+};
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const flowerId = context.params?.flowerId;
+export const getStaticProps: GetStaticProps = async (context) => {
+  const flowerId = context.params?.plantId;
 
   if (!flowerId) {
     return {
@@ -29,20 +149,48 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     };
   }
 
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
   try {
-    const response = await axios.get(`/api/flowers/${flowerId}`);
-    const flower = response.data;
+    const response = await fetch(`${apiUrl}/api/flowers/${flowerId}`);
+    const flower = await response.json();
 
     return {
       props: {
         flower,
       },
+      revalidate: 60, // Optional: revalidate the data every 60 seconds
     };
   } catch (error) {
     console.error(error);
 
     return {
       notFound: true,
+    };
+  }
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+  try {
+    const response = await fetch(`${apiUrl}/api/flowers`);
+    const flowers = await response.json();
+
+    const paths = flowers.map((flower: FlowerInterface) => ({
+      params: { plantId: flower.id },
+    }));
+
+    return {
+      paths,
+      fallback: 'blocking',
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      paths: [],
+      fallback: 'blocking',
     };
   }
 };
