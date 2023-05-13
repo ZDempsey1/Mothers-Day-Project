b0a8cf77cbfbb6a4b13bff835768e9792bd3b6cc
zach <zachartydempsey@gmail.com.here>
2023-05-13 09:04:17 -0500

    linter for deploy

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -1,15 +1,33 @@
-import useSwr from 'swr'
+// import useSwr from 'swr'
 
+// import fetcher from '@/libs/fetcher';
+
+// const useCurrentUser = () => {
+//   const { data, error, isLoading, mutate } = useSwr('/api/current', fetcher);
+//   return {
+//     data,
+//     error,
+//     isLoading,
+//     mutate,
+//   }
+// };
+
+// export default useCurrentUser;
+import useSWR from 'swr';
 import fetcher from '@/libs/fetcher';
 
 const useCurrentUser = () => {
-  const { data, error, isLoading, mutate } = useSwr('/api/current', fetcher);
+  const { data, error } = useSWR('/api/current', fetcher);
+
+  const isLoading = !data && !error;
+  const status = isLoading ? 'loading' : error ? 'error' : 'success';
+
   return {
     data,
     error,
     isLoading,
-    mutate,
-  }
+    status,
+  };
 };
 
 export default useCurrentUser;
