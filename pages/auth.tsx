010e9ebdaf5497f66dcd10815e4a9846020c5057
zach <zachartydempsey@gmail.com.here>
2023-05-14 18:59:24 -0500

    added facebook login

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -5,7 +5,7 @@ import { getSession, signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
-
+import { FaFacebook } from 'react-icons/fa';
 import Input from '@/components/Input';
 
 export async function getServerSideProps(context: NextPageContext) {
@@ -120,9 +120,15 @@ const Auth = () => {
               <div onClick={() => signIn('google', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
                 <FcGoogle size={32} />
               </div>
+
+              <div onClick={() => signIn('facebook', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+      <FaFacebook size={32} />
+    </div>
+
               <div onClick={() => signIn('github', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
                 <FaGithub size={32} />
               </div>
+
             </div>
             <p className="text-white text-xl text-center mt-12 flex flex-col">
                     <span>
