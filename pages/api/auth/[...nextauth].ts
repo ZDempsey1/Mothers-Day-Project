2dce450641d71f028d051a88c7343a4a1b315849
zach <zachartydempsey@gmail.com.here>
2023-05-14 18:19:51 -0500

    updated favorites with search bar and started facebook auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,6 +5,7 @@ import Credentials from 'next-auth/providers/credentials';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { compare } from 'bcrypt';
 import prismadb from '@/libs/prismadb';
+import FacebookProvider from "next-auth/providers/facebook";
 
 export const authOptions: AuthOptions = {
   providers: [
@@ -15,6 +16,10 @@ export const authOptions: AuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID || '',
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET || '',
     }),
     Credentials({
       id: 'credentials',
--- pages/favorites.tsx
+++ pages/favorites.tsx
@@ -1,19 +1,76 @@
+// import Navbar from '@/components/Navbar';
+// import useFavorites from '@/hooks/useFavorites';
+// import React from 'react';
+// import FlowerList from '@/components/FlowerList';
+
+// const FavoritesIndex = () => {
+//     const { data: favorites = [] } = useFavorites();
+
+//     return (
+//         <div className="flex flex-col">
+//         <Navbar />
+//         <div className="mt-20">
+//             <FlowerList name="Favorite Plants" data={favorites} />
+//         </div>
+//         </div>
+//     );
+// };
+
+// export default FavoritesIndex;
+
+import React, { useState } from 'react';
 import Navbar from '@/components/Navbar';
 import useFavorites from '@/hooks/useFavorites';
-import React from 'react';
 import FlowerList from '@/components/FlowerList';
+import { FlowerInterface } from '@/types/index';
 
 const FavoritesIndex = () => {
-    const { data: favorites = [] } = useFavorites();
+  const { data: favorites = [] } = useFavorites();
+  const [searchTerm, setSearchTerm] = useState('');
 
-    return (
-        <div className="flex flex-col">
-        <Navbar />
-        <div className="mt-20">
-            <FlowerList name="Favorite Plants" data={favorites} />
-        </div>
-        </div>
-    );
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredFavorites = favorites.filter((flower: FlowerInterface) => {
+    const name = flower.name ? flower.name.toLowerCase() : '';
+    const bName = flower.bName ? flower.bName.toLowerCase() : '';
+    const family = flower.family ? flower.family.toLowerCase() : '';
+    const plantType = flower.plantType ? flower.plantType.toLowerCase() : '';
+
+    return name.includes(searchTerm.toLowerCase()) ||
+          bName.includes(searchTerm.toLowerCase()) ||
+          family.includes(searchTerm.toLowerCase()) ||
+          plantType.includes(searchTerm.toLowerCase());
+  });
+
+  return (
+    <div className="flex flex-col">
+      <Navbar />
+      <div className="mt-20">
+        <FlowerList
+          name="Favorite Plants"
+          data={filteredFavorites}
+          renderHeader={() => (
+            <input
+              type="text"
+              placeholder="Search your favorites by common/botanical name, plant type, or plant family."
+              value={searchTerm}
+              onChange={handleSearchChange}
+              className="your-input-class mb-10 mt-3"
+              style={{
+                height: '40px',
+                width: '100%',
+                maxWidth: '660px',
+                padding: '8px',
+                boxSizing: 'border-box',
+              }}
+            />
+          )}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default FavoritesIndex;
